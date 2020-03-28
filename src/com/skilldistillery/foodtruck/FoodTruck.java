da87ac38de8b12cc75e72f05c1d5b42325c81889
arpapp <trpapp@gmail.com>
2020-03-28 11:58:08 -0600

    creates food truck objects and stores them in array. Includes menu

--- src/com/skilldistillery/foodtruck/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/FoodTruck.java
@@ -1,5 +1,49 @@
 package com.skilldistillery.foodtruck;
 
 public class FoodTruck {
+	
+	private String name;
+	private String cuisine;
+	private double rating;
+	private int truckID;
+	private static int MAX_TRUCKS = 5;
+	
+	public FoodTruck (String name, String cuisine, double rating) {
+		this.name = name;
+		this.cuisine = cuisine;
+		this.rating = rating;
+		this.truckID = truckID;
+	}
+//no ID in constructor b/c it's not passed?
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCuisine() {
+		return cuisine;
+	}
+
+	public void setCuisine(String cuisine) {
+		this.cuisine = cuisine;
+	}
+
+	public double getRating() {
+		return rating;
+	}
+
+	public void setRating(double rating) {
+		this.rating = rating;
+	}
+
+	@Override
+	public String toString() {
+		return "FoodTruck [name=" + name + ", cuisine=" + cuisine + ", rating=" + rating + "]";
+	}
+	
+	
 }
--- src/com/skilldistillery/foodtruck/FoodTruckApplication.java
+++ src/com/skilldistillery/foodtruck/FoodTruckApplication.java
@@ -1,9 +1,40 @@
 package com.skilldistillery.foodtruck;
 
+import java.util.Scanner;
+
 public class FoodTruckApplication {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner kb = new Scanner(System.in);
+		
+//		FoodTruckApplication FT = new FoodTruckApplication(); //IDK Denise did this for cars
+		
+		
+		FoodTruck[] groupTruck = new FoodTruck[5]; // array creation
+
+		System.out.println("Welcome to Food Truck Rater!\n You will be able to rate up to five trucks."
+				+ "\n To quit, please type \"quit\". ");
+		
+		for (int i = 0; i < groupTruck.length; i++) {
+		System.out.print("Please enter the name of the food truck: ");
+		String name = kb.nextLine();
+
+		System.out.print("Please enter the cuisine/food type: ");
+		String cuisine = kb.nextLine();
+
+		System.out.print("Please enter your rating for the truck, (scale of 1 - 10); ");
+		double rating = kb.nextDouble();
+		kb.nextLine();
+		
+		FoodTruck truck = new FoodTruck(name, cuisine, rating); //object creation
+		groupTruck [i] = truck;
+		
+		}
+		
+		System.out.println("" + groupTruck[0] + groupTruck[1] + groupTruck[2] + groupTruck[3] + groupTruck[4]) ;
+		
+		
+		
 
 	}
 
